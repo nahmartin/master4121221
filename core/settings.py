3aacd7bc2e7015e91a65f82238c42123f804d185
allen <allen@gmail.com>
2023-10-30 19:40:35 +0200

    changed the emails

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,7 @@ if not SECRET_KEY:
     SECRET_KEY = ''.join(random.choice(string.ascii_lowercase) for i in range(32))
 
 # Render Deployment Code
-DEBUG = 'True'
+DEBUG = 'False'
 
 # Docker HOST
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
